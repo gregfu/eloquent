6dbf7ec2e710508db1c2af28f7f4209f10d4d755
Greg Furmanek <greg.furmanek@gmail.com>
2012-05-22 00:20:46 -0700

    Refactoring of article spec

--- lib/eloquent/article.rb
+++ lib/eloquent/article.rb
@@ -11,13 +11,18 @@ module Eloquent
     def save
       FileUtils.mkdir_p("articles/#{@title}")
       FileUtils.mkdir_p("articles/#{@title}/assets")
-      File.open("articles/#{@title}/#{@title}.md", "w") do |f|
+      File.open(output_file, "w") do |f|
         current_time = Time.now
         title = @title
         subtitle = @options[:subtitle]
         f.write(ERB.new(IO.read(template)).result(binding))
       end
       STDOUT.puts("Article #{@title} has been created")
+      return self
+    end
+
+    def output_file
+      "articles/#{@title}/#{@title}.md"
     end
   end
 end
--- spec/eloquent/article_spec.rb
+++ spec/eloquent/article_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 
 describe Eloquent::Article do
+  let(:article_class) { Eloquent::Article }
+
   it "can be initialized" do
-    article = Eloquent::Article.new("title", {})
+    article = article_class.new("title", {})
     article.should be
   end
 
   it "can be saved" do
-    article = Eloquent::Article.new("test_title", {})
-    article.save
+    article = article_class.new("test_title", {})
+    article.save.should be
   end
 
   it "can be generated from template" do
-    article = Eloquent::Article.new("test_title", {:template => "foo"})
+    article = article_class.new("test_title", {:template => "foo"}).save
+    File.exist?(article.output_file).should be
   end
 end
